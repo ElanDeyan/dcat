9b697d9e4918e8612cac8c6eed4cff0e70e8f984
Elan Deyan da Silva Almeida <elandeyandasilva@gmail.com>
2024-10-12 22:24:15 -0300

    refactor: removes unnecessary parameter in the StandardInputArgument and adds templates for docs and docs for the file getter

--- lib/core/types/argument_value.dart
+++ lib/core/types/argument_value.dart
@@ -7,10 +7,13 @@ sealed class DcatArgumentValue {
   const DcatArgumentValue();
 }
 
+/// {@template regular_file_argument}
 /// Class that represents an existent file.
 ///
 /// If file not exists, throws [MissingFileException].
+/// {@endtemplate}
 final class RegularFileArgument extends DcatArgumentValue {
+  /// {@macro regular_file_argument}
   RegularFileArgument(this._file) {
     if (!_file.existsSync()) {
       throw MissingFileException('Inexistent file: ${_file.path}');
@@ -18,21 +21,16 @@ final class RegularFileArgument extends DcatArgumentValue {
   }
   final File _file;
 
+  /// The [File] passed as argument.
   File get file => _file;
 }
 
+/// {@template standard_input_argument}
 /// Represents an input for reading the standard input.
 ///
 /// If not empty string a single dash, throws [FormatException].
+/// {@endtemplate}
 final class StandardInputArgument extends DcatArgumentValue {
-  StandardInputArgument(String input)
-      : _input = input == '' || input == '-'
-            ? input
-            : throw FormatException(
-                'Should be an empty string or dash',
-                input,
-              );
-  final String _input;
-
-  String get input => _input;
+  /// {@macro standard_input_argument}
+  const StandardInputArgument();
 }
